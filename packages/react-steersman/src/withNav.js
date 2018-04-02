2543572dfdbbd46e53f54e86b8d61755d73f8bb0
3axap4eHko <3axap4eHko@gmail.com>
2018-04-02 16:14:20 -0400

    v4.0.0-1

--- packages/react-steersman/src/withNav.js
+++ packages/react-steersman/src/withNav.js
@@ -1,25 +1,25 @@
 import React, { Component as ReactComponent } from 'react';
 import { object } from 'prop-types';
+import { withContext } from './Steersman';
 import navigateTo from './navigateTo';
 
 export default function withNav(Component) {
 
   const componentName = Component.displayName || Component.name;
 
-  return class NavigatedComponent extends ReactComponent {
+  @withContext
+  class NavigatedComponent extends ReactComponent {
     static displayName = `Navigated(${componentName})`;
     static WrappedComponent = Component;
 
-    static contextTypes = {
-      history: object.isRequired,
-    };
-
     navigate = (path = 0) => {
-      navigateTo(this.context.history, path);
+      navigateTo(this.props.steersman.history, path);
     };
 
     render() {
       return <Component {...this.props} navigate={this.navigate} />;
     }
-  };
+  }
+
+  return NavigatedComponent;
 }
